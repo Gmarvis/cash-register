377f230fd309f8d774adfaef807d856f484b7b62
gmarvis <sgmarvis@gmail.com>
2023-07-31 16:45:08 +0100

    started new the solution with a new approach

--- index.js
+++ index.js
@@ -1,66 +1,91 @@
 let cid = [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60], 
-]
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+];
 
-let cashCashRegster = (price, cash, cid)=>{
-    let change = cash - price
-    statusResults = {
-        status: "",
-        changeCid: []
-    }
+const checkCashRegister = (price, cash, cid) => {
+  console.log(cid);
+};
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
 
-    let sumCid = 0
-    for (let i = 0; i < cid.length; i++){
-        sumCid+=cid[i][1]
-    }
-    console.log('sumCid =',sumCid)
-    console.log('change =',change)
+// let cashCashRegster = (price, cash, cid)=>{
+//     let change = cash - price
+//     statusResults = {
+//         status: "",
+//         changeCid: []
+//     }
 
-    // 1st condition check if ther is cash is avialeble in cid
+//     let sumCid = 0
+//     for (let i = 0; i < cid.length; i++){
+//         sumCid+=cid[i][1].toFixed(2)
+//     }
+//     console.log('sumCid =',sumCid)
+//     console.log('change =',change)
 
-    if (sumCid < change && sumCid > 0){
-        statusResults.status = "insufficient funds"
-    }
+//     // 1st condition check if ther is cash is avialeble in cid
 
-    // 2nd condition if sum of cid is equal to change 
+//     if (sumCid < change){
+//         statusResults.status = "insufficient funds"
+//         console.log("statusResults =", statusResults)
+//     }
 
-    else if (sumCid === change){
-        statusResults.status = "closed"
-        statusResults.status = cid
-    }
+//     // 2nd condition if sum of cid is equal to change
 
+//     else if (sumCid === change){
+//         statusResults.status = "closed"
+//         statusResults.change = cid
+//         console.log("statusResults = ", statusResults)
+//     }
 
-    // 3rd condition 
-    else{
-    let newCid = []
-    let newChange = 0
+//     // 3rd condition
+//     else{
+//     let newCid = [] // newCid is the array to collect the bank note names and thier coresponding values.
+//     let newChange = 0 //  this will constantly be updadating as we look for clients change.
 
-    let changeWhole = Math.floor(change)
-    let flootchange = change - changeWhole
+//     let changeWhole = Math.floor(change) // changeWhole is our change minus floot numbers
+//     let flootchange = change - changeWhole // flootnumbers
 
+//     console.log("changeWhole =", changeWhole)
+//     console.log("flootchange =", flootchange)
 
-    console.log("changeWhole =", changeWhole)
-    console.log("flootchange =", flootchange)
+//     // check the multiples for changeWhole numbers
 
-    // check the multiples for changeWhole numbers 
+//     let multiOfChangeWhole = Math.floor(changeWhole/20)
+//     let afterMult =  multiOfChangeWhole*20
 
-    multiOfChangeWhole = Math.floor(changeWhole/20)
-    let afterMult =  multiOfChangeWhole*20
+//     let chRem = changeWhole%20
+//     console.log("chRem",chRem)
+//     console.log("--------------------------------------")
 
-    console.log("multiOfChangeWhole = ", multiOfChangeWhole)
-    console.log("afterMult =",afterMult)
+//     console.log("multiOfChangeWhole = ", multiOfChangeWhole)
+//     console.log("afterMult =",afterMult)
 
+//     console.log("-----------------------------------------------------------------")
+//     if (afterMult>=cid[7][1]){
+//         let removeCash = afterMult - cid[7][1]
+//         cid[7][1]=afterMult-removeCash
+//         newChange = removeCash + chRem
 
+//         console.log("removeCash=",removeCash)
+//         console.log("newChange =", newChange)
+//     }
 
-
-
-}
-}
-cashCashRegster(3.26, 100, cid)
+// }
+// }
+// cashCashRegster(3.26, 100, cid)
